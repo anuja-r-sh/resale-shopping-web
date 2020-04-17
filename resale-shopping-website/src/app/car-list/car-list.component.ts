793eaaaa1d2afc9727e9bca4af81db76f5b59d68
Anuja <anuja.shingne@tcs.com>
2020-04-17 13:38:42 +0200

    Added car list compoent and injected the service to fetch cars list

--- resale-shopping-website/src/app/car-list/car-list.component.ts
+++ resale-shopping-website/src/app/car-list/car-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Pipe, Component, OnInit } from '@angular/core';
+
+import { ResaleService } from '../services/resale.service';
+import { CarList } from '../model/car';
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.css']
+  styleUrls: ['./car-list.component.css'],
 })
 export class CarListComponent implements OnInit {
 
-  constructor() { }
+  carList: CarList;
+  present: String;
+  constructor(private resaleService: ResaleService, ) { }
 
   ngOnInit(): void {
+    
+    this.resaleService.getAllCarsOfWareHouse().subscribe(data => {
+
+      this.carList = data;
+    });
+  }
+
+  getLicenseStatus(flag){
+  return  (flag) ? "License Available" : "License Not Available";
+
   }
 
 }
