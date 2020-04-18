66e9ac5fd5735cd8c3ac1ca379c9663b6fd5a209
Anuja <anuja.shingne@tcs.com>
2020-04-18 11:22:22 +0200

    UI refactor for Car list container

--- resale-shopping-website/src/app/car/car.component.ts
+++ resale-shopping-website/src/app/car/car.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Car } from '../model/car'
 import { WareHouse } from '../model/wareHouse';
 import { ResaleService } from '../services/resale.service';
+import { InteractionService } from '../services/interaction.service';
+
 
 @Component({
   selector: 'app-car',
@@ -13,10 +15,14 @@ export class CarComponent implements OnInit {
   @Input() car: Car;
   wareHouse: WareHouse;
   isCollapsed: boolean = true;
+  isAddedToCart: boolean = false;
 
-  constructor(private resaleService: ResaleService, ) { }
+  constructor(private resaleService: ResaleService, private interactionService: InteractionService) { }
 
   ngOnInit(): void {
+    this.interactionService.cartItems$.subscribe(car =>{
+
+    });
   }
 
   getDateAdded(dateAdded) {
@@ -37,5 +43,14 @@ export class CarComponent implements OnInit {
   }
   getMapLocation(lat, long) {
     return `http://www.google.com/maps/place/${lat},${long}`;
+  }
+
+  addToCart(car) {
+    this.isAddedToCart = true;
+    this.interactionService.addToCart(car);
+  }
+
+  removeFromCart(car) {
+    this.interactionService.removeFromCart(car);
   }
 }
